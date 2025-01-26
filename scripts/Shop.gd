6b73774477176f50f04c141f859f1efede2a8f65
JesseIguess <blueberry21007@gmail.com>
2025-01-26 19:23:33 +0000

    efededed

---
+++ scripts/Shop.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+# @onready @export
+
+func _process(delta):
+	# time_elapsed += delta
+	# $Sprite2D.position.y = round((sin(time_elapsed * 15) + 1) / 2) * 0.5
+
+func toggle_shop(body):
+	if body.name == "Player":
+        # GlobalVars.shop_visility = 
+		pass
