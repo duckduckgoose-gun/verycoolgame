0fd6b36291a6965a7b27b582f0f23d2dcb76a190
JesseIguess <blueberry21007@gmail.com>
2025-01-26 04:03:08 +0000

    iuwrhfgfiuwrhgiuwrg

--- scripts/CSStudents.gd
+++ scripts/CSStudents.gd
@@ -3,9 +3,9 @@ extends CharacterBody2D
 const movement_vectors = [
 	Vector2(1, 0), Vector2(-1, 0), Vector2(0, 1), Vector2(0, -1)
 ]
-const trash_names = [
-    "cola", "sprite", "pepsi",
-    "welchs", "welchs"
+const trash_names : Array[String] = [
+	"cola", "sprite", "pepsi",
+	"welchs", "welchs"
 ]
 
 var vector_index = 0;
@@ -17,37 +17,38 @@ var vector_index = 0;
 
 var cur_trash_time = 0
 var trash_timer = 0
-var steal_timer = 0
+var steal_timer = 999999999
 
 func _ready():
-    cur_trash_time = trash_time + trash_margin * (2 * randfn() - 1)
-    vector_index = randi() % 4
+	cur_trash_time = trash_time + trash_margin * (2 * randf() - 1)
+	vector_index = randi() % 4
 
 func _physics_process(delta):
 	var collision = move_and_collide(movement_vectors[vector_index] * speed * delta)
-    steal_cooldown += delta
+	steal_timer += delta
 	if collision:
 		_body_entered()
 
-        const collider = collision.get_collider()
-        if collider is Chair: collider.pull_out()
-        if collider.name == "Player" and steal_timer >= steal_cooldown:
-            GlobalVars.add_coins(-3)
-            steal_timer = 0
+		var collider = collision.get_collider()
+		
+		if collider is Chair: collider.pull_out()
+		elif collider.name == "Player" and steal_timer >= steal_cooldown:
+			GlobalVars.add_coins(-3)
+			steal_timer = 0
 
-    if trash_timer >= cur_trash_time:
-        create_trash()
-    else:
-        trash_timer += delta
+	if trash_timer >= cur_trash_time:
+		create_trash()
+	else:
+		trash_timer += delta
 
 func create_trash():
-    cur_trash_time = trash_time + trash_margin * (2 * randfn() - 1)
-    trash_timer = 0
-    # spawn trash lmao
-    var trash_name = trash_names[randi() % trash_names.length()];
-    var new_trash = get_tree().call_deferred("add_child", load("res://trash/" + trash_name + ".tscn").instantiate())
-    new_trash.position = position
-    
+	cur_trash_time = trash_time + trash_margin * (2 * randf() - 1)
+	trash_timer = 0
+	# spawn trash lmao
+	#var trash_name = trash_names[randi() % trash_names.size()];
+	var new_trash = get_tree().call_deferred("add_child", load("res://trash.tscn").instantiate())
+	new_trash.position = position
+	
 
 func _body_entered():
 	# change movement
--- scripts/Chair.gd
+++ scripts/Chair.gd
@@ -1,16 +1,17 @@
 extends StaticBody2D
+class_name Chair
 
-const forward_directions = [
-    Vector2(0, 1), Vector2(1, 0), Vector2(0, -1), Vector2(-1, 0)
+const forward_directions : Array[Vector2] = [
+	Vector2(0, 1), Vector2(1, 0), Vector2(0, -1), Vector2(-1, 0)
 ]
 @export var direction : int = 2
 
 var pulled_out : bool = false
 
 func pull_out():
-    if !pulled_out:
-        position -= forward_directions
+	if !pulled_out:
+		position -= forward_directions[direction]
 
 func push_in():
-    if pulled_out:
-        position += forward_directions
+	if pulled_out:
+		position += forward_directions[direction]
