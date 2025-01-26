994bf3b431f84367734091bb1df3586b602ca6d8
JesseIguess <blueberry21007@gmail.com>
2025-01-26 02:08:50 +0000

    hehe

--- scripts/CSStudents.gd
+++ scripts/CSStudents.gd
@@ -3,6 +3,11 @@ extends CharacterBody2D
 const movement_vectors = [
 	Vector2(1, 0), Vector2(-1, 0), Vector2(0, 1), Vector2(0, -1)
 ]
+const trash_names = [
+    "cola", "sprite", "pepsi",
+    "welchs", "welchs"
+]
+
 var vector_index = 0;
 @export var speed : float
 @export var trash_time : float = 4.5
@@ -21,9 +26,18 @@ func _physics_process(delta):
 	if collision:
 		_body_entered()
 
+    if trash_timer >= cur_trash_time:
+        create_trash()
+    else:
+        trash_timer += delta
+
 func create_trash():
     cur_trash_time = trash_time + trash_margin * (2 * randfn() - 1)
+    trash_timer = 0
     # spawn trash lmao
+    var trash_name = trash_names[randi() % trash_names.length()];
+    var new_trash = get_tree().call_deferred("add_child", load("res://trash/" + trash_name + ".tscn").instantiate())
+    new_trash.position = position
     
 
 func _body_entered():
